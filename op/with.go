4815651eb2dac56eae41076ff39feb0d9396ab87
Maxim Oransky <maxim.oransky@gmail.com>
2020-12-19 17:40:55 +0300

    WithQuery

--- op/with.go
+++ op/with.go
@@ -2,18 +2,18 @@ package op
 
 import "fmt"
 
-type with string
+type WithQuery string
 
-func With(alias string, q Query) with {
-	return with(fmt.Sprintf("WITH %s AS (%s)", alias, q))
+func With(alias string, q Query) WithQuery {
+	return WithQuery(fmt.Sprintf("WITH %s AS (%s)", alias, q))
 }
 
-func (w with) String() string { return string(w) }
+func (w WithQuery) String() string { return string(w) }
 
-func (w with) With(alias string, q Query) with {
-	return with(fmt.Sprintf("%s, %s AS (%s)", w, alias, q))
+func (w WithQuery) With(alias string, q Query) WithQuery {
+	return WithQuery(fmt.Sprintf("%s, %s AS (%s)", w, alias, q))
 }
 
-func (w with) Do(q Expr) Query {
+func (w WithQuery) Do(q Expr) Query {
 	return RawQuery(w, q)
 }
