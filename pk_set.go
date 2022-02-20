c0f8cee9e379952f8e6b83919cfa3acb5c93b715
smee <maxim.oransky@gmail.com>
2022-02-21 00:22:08 +0300

    lorm.Set fix

--- pk_set.go
+++ pk_set.go
@@ -1,7 +1,7 @@
 package lorm
 
 type iPK interface {
-	int | int64 | string
+	~int | ~int64 | ~string
 }
 
 type Set[PK iPK] map[PK]struct{}
@@ -11,8 +11,7 @@ func (s Set[PK]) Add(pk PK) { s[pk] = struct{}{} }
 func (s Set[PK]) Contains(pk PK) bool { _, ok := s[pk]; return ok }
 
 func (s Set[PK]) AsList() []PK {
-	//pks := make([]PK, 0, len(s)) <-- len(s) doesn't work yet
-	pks := make([]PK, 0)
+	pks := make([]PK, 0, len(s))
 	for pk := range s {
 		pks = append(pks, pk)
 	}
